434c1b73d0320667f7bb33ce2fe3b5ca7bbdec78
ysmaicon <94692654+ysmaicon@users.noreply.github.com>
2021-12-03 17:57:07 -0300

    Update RoundRobin.c

--- RoundRobin.c
+++ RoundRobin.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // Struct para guardar cada processo com sua criação, duração e prioridade
 typedef struct {
     int criacao, duracao, prioridade;
@@ -10,16 +9,16 @@ typedef struct {
 
 processo *vet;
 
-int n, cont_saida = 0;
+int n, cont_saida = 0, time = 0;
 int conta_proc = 0, total_time = 0;
-int *burst_time, *arrival_time, *total_tp;
+int *burst_time, *arrival_time, *total_tp, *order_process;
 int *wait_tp, *life_tp;
 float average_wait_time, average_life_time;
 
 // Função para leitura da entrada
 void dados() {
 
-    // Leitura e atribuição dos dados de um arquivo "dados.txt" para um vetor dinamicamente alocado. Nesse arquivo,  
+    // Leitura e atribuição dos dados de um arquivo "dados.txt" para um vetor dinamicamente alocado. Nesse arquivo,
     // as três colunas são separadas por um espaço e não pode existir linhas vazias.
     int i,j;
     char car;
@@ -44,7 +43,7 @@ void dados() {
         for(i = 0; i < n; i++)
             fscanf(arq, "%d %d %d\n", &vet[i].criacao, &vet[i].duracao, &vet[i].prioridade);
         fclose(arq);
-        
+
         // Reserva espaço para armazenas a duração de cada processo e seus tempos de chegada na fila
         // de processos, respectivamente
         burst_time = (int *) malloc(n*sizeof(int));
@@ -58,6 +57,8 @@ void dados() {
             arrival_time[i] = vet[i].criacao;
         }
 
+        printf("\n");
+
         // Número total de processos
         conta_proc = n;
     }
@@ -65,19 +66,41 @@ void dados() {
 
 void saida() {
 
-    int i;
+    int i, j;
     FILE *arq;
 
     // Criação e atribuição de dados para um arquivo "saida.txt"
     arq = fopen("saida.txt", "w");
     if(arq == NULL)
         printf("Nao foi possivel abrir o arquivo");
-    else{ 
+    else{
         // Armazena os valores no arquivo de texto e também imprime na tela
-        fprintf(arq, "ID do processo \t\t Tempo de Duracao \t\t Tempo de Vida \t\t Tempo de Espera");
+
+        fprintf(arq,"Tempo \t");
+        for (i = 0; i < conta_proc; i++)
+        {
+            fprintf(arq, " P%d \t",i+1);
+        }
+
+        fprintf(arq, "\n");
+
+        for(i = 0; i < time; i++)
+        {
+            fprintf(arq, "%d-%d \t ",i,i+1);
+            for (j = 0; j < conta_proc; j++)
+            {
+                if(order_process[i] == j)
+                    fprintf(arq, "## \t");
+                else
+                    fprintf(arq, "-- \t");
+            }
+            fprintf(arq, "\n");
+        }
+
+        fprintf(arq, "\nID do processo \t\t Tempo de Duracao \t\t Tempo de Vida \t\t Tempo de Espera");
         printf("ID do processo \t\t Tempo de Duracao \t\t Tempo de Vida \t\t Tempo de Espera");
         fseek (arq, 2, SEEK_CUR);
-        for (i = 0; i < conta_proc; i++) 
+        for (i = 0; i < conta_proc; i++)
         {
             fprintf(arq, "\nProcesso [%d] \t\t\t\t %d \t\t\t\t %d \t\t\t %d", (i + 1), (vet[i].duracao), (total_tp[i] - arrival_time[i]), (total_tp[i] - arrival_time[i] - vet[i].duracao));
             printf("\nProcesso [%d] \t\t\t\t %d \t\t\t\t %d \t\t\t %d", (i + 1), (vet[i].duracao), (total_tp[i] - arrival_time[i]), (total_tp[i] - arrival_time[i] - vet[i].duracao));
@@ -96,34 +119,40 @@ void saida() {
 // Função para escalonamento dos processos através do algoritmo Round-Robin
 void roundrobin() {
 
-    // Variáveis: 
-    // i -> para o loop for; 
+    // Variáveis:
+    // i -> para o loop for;
+    // time -> instante de tempo;
     // max_proc -> guarda o número máximo de processos;
     // terminado -> flag que indica se o processo foi completamente executado;
-    // quantum -> guarda o valor do quantum usado para a construção do algoritmo
-    // wait_time -> tempo de espera dos processos
-    // life_time -> tempo de vida dos processos
+    // quantum -> guarda o valor do quantum usado para a construção do algoritmo;
+    // wait_time -> tempo de espera dos processos;
+    // life_time -> tempo de vida dos processos;
 
     int i, j, max_proc, terminado = 0, quantum;
     int wait_time = 0, life_time = 0;
 
     max_proc = conta_proc;
-    quantum = 2;
+    quantum = 1;
 
     // Aloca espaço para guardar os tempos totais, de espera e de vida dos processos, respectivamente
     total_tp = (int *) malloc(n*sizeof(int));
     wait_tp = (int *) malloc(n*sizeof(int));
     life_tp = (int *) malloc(n*sizeof(int));
+    order_process = (int *) malloc(10*n*sizeof(int));
 
     // Para cada processo da fila
     for(total_time = 0, i = 0; max_proc != 0;)
     {
+        //registra qual processo está sendo executado em cada instante
+        order_process[time++] = i;
+
         // Se a duração não for nula ou, por proteção da entrada, negativa,
         // e se a duração do processo for menor ou igual ao quantum determinado
         if(burst_time[i] <= quantum && burst_time[i] > 0)
         {
             // O tempo total para a execução do processo é igual ao tempo
             // que se passou de outros processos anteriores + sua própria duração
+
             total_time = total_time + burst_time[i];
             burst_time[i] = 0;
             terminado = 1;
@@ -148,7 +177,9 @@ void roundrobin() {
 
             // O tempo de espera conta o tempo total e tira dele
             // o tempo de chegada dele na fila e sua duração
+            //printf("(%d+%d)-%d-%d",wait_time, total_time, arrival_time[i],vet[i].duracao);
             wait_time = wait_time + total_time - arrival_time[i] - vet[i].duracao;
+            //printf("=%d\n",wait_time);
 
             // O tempo de vida conta o tempo total e tira dele
             // o tempo de chegada dele na fila
